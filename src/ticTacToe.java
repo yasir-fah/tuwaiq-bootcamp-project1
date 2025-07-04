1dfe5b62f97e1181a0d2f1850e254ad4f5ddaae8
dev <dev@test.com>
2025-07-04 13:49:53 +0300

    add diagonal win - 1 player only

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -59,20 +59,34 @@ public class ticTacToe {
 //    }
 
     public static boolean isWin(String[][] matrix, String choice) {
-        int counterHorizental = 0;
+        int counterHorizontal = 0;
         int counterVertical   = 0;
+        int counterDiagonal1  = 0;
+        int counterDiagonal2  = 0;
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
+                //1- Horizontal
                 if(matrix[i][j].contains(choice)){
-                    counterHorizental++; // counter = 3 => win.
+                    counterHorizontal++; // counter = 3 => win.
                 }else{
-                    counterHorizental = 0;
+                    counterHorizontal = 0;
                 }
+                //2- vertical
                 if(matrix[j][i].contains(choice)){
                     counterVertical++;
                 }else{
                     counterVertical = 0;
+                }
+                //3- diagonal
+                if(i == j){
+                   if(matrix[i][j].contains(choice)){
+                       counterDiagonal1++;
+                   }
+                }else if(i+2 == 2){
+                    if(matrix[i][j].contains(choice)){
+                        counterDiagonal2++;
+                    }
                 }
 
                 //when reach to end of a row and no win
@@ -85,18 +99,24 @@ public class ticTacToe {
 
             //when reach to end of a column and no win
             if(i == 2){
-                if(counterHorizental < 3){
-                    counterHorizental = 0;
+                if(counterHorizontal < 3){
+                    counterHorizontal = 0;
                 }
             }
 
             // when player hit a line and win
-            if(counterHorizental == 3){
-                System.out.println("win !");
+            if(counterHorizontal == 3){
+                System.out.println("win !, horizontally");
                 return true;
 
             }else if(counterVertical == 3){
-                System.out.println("win !");
+                System.out.println("win !, vertically");
+                return true;
+            }else if(counterDiagonal1 == 3){
+                System.out.println("win !, diagonally");
+                return true;
+            }else if(counterDiagonal2 == 3){
+                System.out.println("win !, diagonally");
                 return true;
             }
 
