2bd4e92ca708a34fb287fa9029ff504a97420fb9
dev <dev@test.com>
2025-07-04 18:29:35 +0300

    add second player(computer) with it's functionality

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -14,10 +14,15 @@ public class ticTacToe {
         String userChoice = input.nextLine(); //e.g  X
 
         int counter = 0 ;
+        boolean win = false;
+        boolean validPos = false;
+        String computerVariable;
+
         int userRow;
         int userCol;
-        boolean win = false;
-        boolean validPos ;
+
+        int compRow;
+        int compCol;
 
 
         // make it empty matrix until user add position
@@ -42,21 +47,60 @@ public class ticTacToe {
                 userCol = input.nextInt();
 
                 /// if is valid or not:
-                validPos = isValidPos(ticTacToeMatrix,userRow,userCol);
 
+                    validPos = isValidPos(ticTacToeMatrix, userRow, userCol);
+                    if(!validPos){
+                        System.out.println("user chose taken place ");
+                    }
 
             }while(!validPos);
 
+
+            //position confirmed
             ticTacToeMatrix[userRow][userCol] = userChoice;
 
 
             ///check win or not:
             if(counter >= 2){
-               win =  isWin(ticTacToeMatrix,userChoice);
+               win =  isWin(ticTacToeMatrix,userChoice,counter);
+               if(win){
+                   break; // end loop
+               }
             }
 
-            counter++;
+
+            //determine a letter for computer:
+            if(userChoice.toLowerCase().replace(" ","").contains("x")){
+                computerVariable = "O";
+            }else{
+                computerVariable = "X";
+            }
+
+            do {
+                compRow = (int)(Math.random() * 3);
+                compCol = (int)(Math.random() * 3);
+
+
+                    validPos = isValidPos(ticTacToeMatrix,compRow,compCol);
+                    if(!validPos){
+                        System.out.println("computer chose taken place ");
+                }
+
+
+            }while(!validPos);
+
+            ticTacToeMatrix[compRow][compCol] = computerVariable;
+
+            if(counter >= 2){ // call the method only when there is chance of win.
+             win =  isWin(ticTacToeMatrix,computerVariable,counter);
+             if (win){
+                 break; //end loop
+             }
+            }
+
+
         } while (!win);
+        displayBoard(ticTacToeMatrix);
 
     }
 
@@ -74,7 +118,7 @@ public class ticTacToe {
     }
 
 
-    public static boolean isWin(String[][] matrix, String choice) {
+    public static boolean isWin(String[][] matrix, String choice, int counter) {
         int counterHorizontal = 0;
         int counterVertical   = 0;
         int counterDiagonal1  = 0;
@@ -83,61 +127,70 @@ public class ticTacToe {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 //1- Horizontal
-                if(matrix[i][j].contains(choice)){
+                if (matrix[i][j].contains(choice)) {
                     counterHorizontal++; // counter = 3 => win.
-                }else{
+                }
+                else {
                     counterHorizontal = 0;
                 }
                 //2- vertical
-                if(matrix[j][i].contains(choice)){
+                if (matrix[j][i].contains(choice)) {
                     counterVertical++;
-                }else{
+                }
+                else {
                     counterVertical = 0;
                 }
                 //3- diagonal
-                if(i == j){
-                   if(matrix[i][j].contains(choice)){
-                       counterDiagonal1++;
-                   }
-                }else if(i+2 == 2){
-                    if(matrix[i][j].contains(choice)){
+                if (i == j) {
+                    if (matrix[i][j].contains(choice)) {
+                        counterDiagonal1++;
+                    }
+                }
+                else if (i + 2 == 2) {
+                    if (matrix[i][j].contains(choice)) {
                         counterDiagonal2++;
                     }
                 }
 
                 //when reach to end of a row and no win
-                if(j == 2){
-                    if(counterVertical <3){
+                if (j == 2) {
+                    if (counterVertical < 3) {
                         counterVertical = 0;
                     }
                 }
             }
 
             //when reach to end of a column and no win
-            if(i == 2){
-                if(counterHorizontal < 3){
+            if (i == 2) {
+                if (counterHorizontal < 3) {
                     counterHorizontal = 0;
                 }
             }
 
             // when player hit a line and win
-            if(counterHorizontal == 3){
+            if (counterHorizontal == 3) {
                 System.out.println("win !, horizontally");
                 return true;
-
-            }else if(counterVertical == 3){
+            }
+            else if (counterVertical == 3) {
                 System.out.println("win !, vertically");
                 return true;
-            }else if(counterDiagonal1 == 3){
+            }
+            else if (counterDiagonal1 == 3) {
                 System.out.println("win !, diagonally");
                 return true;
-            }else if(counterDiagonal2 == 3){
+            }
+            else if (counterDiagonal2 == 3) {
                 System.out.println("win !, diagonally");
+                return true;
+            }
+            else if (counter >= 8) { // to detect the draw
+                System.out.println("draw");
                 return true;
             }
 
         }
-       return false;
+        return false;
     }
 
 
